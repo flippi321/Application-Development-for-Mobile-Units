66bab3e6af0996f9ef5195fa9de064ce8a27b3de
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-08 17:10:15 +0200

    Modified Main Activity to use Intents

--- Task2/app/src/main/java/com/example/task2/MainActivity.kt
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -3,12 +3,7 @@ package com.example.task2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 class MainActivity : Activity() {
     private val maxNumber = 100
@@ -16,18 +11,11 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Start Randomizer activity
-        val intent = Intent("RandomNumberActivity")
-
-        /* Removed because I don't know wtf this is
-        * intent.putExtra("flag", flagValue)
-        */
-
-        //startActivityForResult(intent, maxNumber)
     }
 
-    fun onClickGenerateRandomNumber(v: View?){
-        Log.w("Warning", "Test")
+    fun onClickGenerateRandomNumber(v: View?) {
+        val intent = Intent(this, RandomNumberActivity::class.java)
+        intent.putExtra("maxValue", maxNumber)
+        startActivity(intent)
     }
 }
