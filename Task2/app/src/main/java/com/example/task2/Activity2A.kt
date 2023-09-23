06c5b4261b7007dc6aeeb70005676e80fc24cb33
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-24 00:32:38 +0200

    Renamed MainActivity to Activity2A

--- Task2/app/src/main/java/com/example/task2/Activity2A.kt
+++ Task2/app/src/main/java/com/example/task2/Activity2A.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : Activity() {
+class Activity2A : Activity() {
     private val maxNumber = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
