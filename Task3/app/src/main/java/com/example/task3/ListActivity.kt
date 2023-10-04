8d8fcb36dd962ae8d0c36136e4a01e2846f3daaa
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 12:22:42 +0200

    Modified ListActivity to have an add-button sending the user to the RegisterActivity

--- Task3/app/src/main/java/com/example/task3/ListActivity.kt
+++ Task3/app/src/main/java/com/example/task3/ListActivity.kt
@@ -1,26 +1,47 @@
-package com.example.task3;
+package com.example.task3
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListActivity : Activity() {
 
     private lateinit var venner: MutableList<Venn>
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        venner = intent.getParcelableArrayListExtra("venner")!!
-        val listView = findViewById<ListView>(R.id.listVenner)
+        venner = mutableListOf()
+        listView = findViewById(R.id.listVenner)
+
         val adapter = VennAdapter(this, venner)
         listView.adapter = adapter
 
+        findViewById<FloatingActionButton>(R.id.fabAddVenn).setOnClickListener {
+            val intent = Intent(this@ListActivity, RegisterActivity::class.java)
+            startActivityForResult(intent, 69)  // Reality can be whatever I want
+        }
+
         listView.setOnItemClickListener { _, _, position, _ ->
-            // Code to edit friend's details
             val selectedVenn = venner[position]
-            // Edit and update the friend's details as needed
+            val intent = Intent(this@ListActivity, RegisterActivity::class.java)
+            intent.putExtra("selectedVenn", selectedVenn)  // Pass the selected Venn to RegisterActivity
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            val newVenn = data?.getParcelableExtra<Venn>("newVenn")
+            if (newVenn != null) {
+                venner.add(newVenn)
+                (listView.adapter as VennAdapter).notifyDataSetChanged()
+            }
         }
     }
 }
