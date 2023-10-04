f0c323c648df47ba7afdd25912554b67aafcff52
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 18:10:01 +0200

    Added option menu code from lecture

--- Task4/app/src/main/java/com/example/task4/MainActivity.kt
+++ Task4/app/src/main/java/com/example/task4/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.task4
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,6 +29,32 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu.add(0, 0, 0,
+            "Fasit").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+        menu.add(0, 1, 0,
+            "Beskrivelse").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        menu.add(0, 2, 0,
+            "Innstillinger").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        menu.add(0, 3, 0,
+            "Avslutt").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            0 -> Toast.makeText(this, "There is no Fasit",
+                Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(this, "Ganske selvbeskrivende",
+                Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "Du har nå akseptert alle Cookies :P",
+                Toast.LENGTH_SHORT).show()
+            3 -> finish()
+            else -> return false
+        }
+        return true
     }
 }
 
