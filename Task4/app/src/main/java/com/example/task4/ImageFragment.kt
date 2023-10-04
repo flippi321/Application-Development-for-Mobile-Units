36a2f72ab95e39ce3006bf1726e171de8062505e
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 20:44:49 +0200

    Renamed Fragments to be more descriptive

--- Task4/app/src/main/java/com/example/task4/ImageFragment.kt
+++ Task4/app/src/main/java/com/example/task4/ImageFragment.kt
@@ -2,7 +2,7 @@ package com.example.task4
 
 import androidx.fragment.app.Fragment
 
-class Fragment1 : Fragment() {
+class ImageFragment : Fragment() {
 
 
 }
--- Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
+++ Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
@@ -2,7 +2,7 @@ package com.example.task4
 
 import androidx.fragment.app.Fragment
 
-class Fragment2 : Fragment() {
+class MovieListFragment : Fragment() {
 
 
 }
