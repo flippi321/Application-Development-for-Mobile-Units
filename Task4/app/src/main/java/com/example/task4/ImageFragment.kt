6fc5b2cbd108be91582544a5eab8a72d907b6fb4
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 21:06:30 +0200

    Added Fragment for displaying the image of a movie

--- Task4/app/src/main/java/com/example/task4/ImageFragment.kt
+++ Task4/app/src/main/java/com/example/task4/ImageFragment.kt
@@ -1,8 +1,27 @@
 package com.example.task4
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.widget.ImageView
+import android.widget.TextView
 
 class ImageFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_image, container, false)
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+        val description: TextView = view.findViewById(R.id.description)
 
+        // TODO FIX
+        imageView.setImageResource(R.drawable.batman)
+        description.text = getString(R.string.some_description)
+
+        return view
+    }
 }
