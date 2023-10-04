fb8f904d39c552ec2f0545584cf9651eee798387
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 20:22:44 +0200

    Modified ActionBar to have a "Forrige" and "Neste" button

--- Task4/app/src/main/java/com/example/task4/MainActivity.kt
+++ Task4/app/src/main/java/com/example/task4/MainActivity.kt
@@ -16,35 +16,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menu.add(0, 0, 0,
-            "Fasit").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
-        menu.add(0, 1, 0,
-            "Beskrivelse").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
-        menu.add(0, 2, 0,
-            "Vis Fasit").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
-        menu.add(0, 3, 0,
-            "Avslutt").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        menu.add(0, 0, 0, "Forrige")
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        menu.add(0, 1, 1, "Neste")
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            0 -> Toast.makeText(this, "Fasit valgt",
-                Toast.LENGTH_SHORT).show()
-            1 -> Toast.makeText(this, "Beskrivelse valgt",
-                Toast.LENGTH_SHORT).show()
-            2 -> showSolutions(true)
-            3 -> finish()
-            else -> return false
+            0 -> Toast.makeText(this, "Forrige clicked", Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(this, "Neste clicked", Toast.LENGTH_SHORT).show()
+            else -> return super.onOptionsItemSelected(item)
         }
         return true
     }
 
     private fun showSolutions(show: Boolean) {
         if (show) {
-            Toast.makeText(this, "Livet og alts betydning er 42",
-                Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Livet og alts betydning er 42", Toast.LENGTH_SHORT).show()
         }
     }
 }
