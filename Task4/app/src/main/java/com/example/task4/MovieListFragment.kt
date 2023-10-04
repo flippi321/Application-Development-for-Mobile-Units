66ff5ab3bb170761fd42f3be08eb8d160b3aa16e
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-04 21:06:43 +0200

    Added a fragment showing a list of multiple movies

--- Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
+++ Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
@@ -1,8 +1,26 @@
 package com.example.task4
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MovieListFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
+        val listView: ListView = view.findViewById(R.id.movieList)
+        val movies = resources.getStringArray(R.array.movies)
 
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, movies)
+        listView.adapter = adapter
+
+        return view
+    }
 }
