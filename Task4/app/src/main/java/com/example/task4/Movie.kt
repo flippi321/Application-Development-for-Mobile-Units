94610148e87e0477e7a4a8b235441963bc10c8cd
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-05 11:18:17 +0200

    Made Movie Parcelable so I can use it in the fragments

--- Task4/app/src/main/java/com/example/task4/Movie.kt
+++ Task4/app/src/main/java/com/example/task4/Movie.kt
@@ -1,7 +1,9 @@
-package com.example.task4
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Movie(
     val name: String,
     val description: String,
-    val imagePath: String
-)
+    val image: String // This can be a URL or a file path, based on your requirement
+) : Parcelable
