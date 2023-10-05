5c6b7b7bfa9addac47d0a8c06d213948536cd4ff
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-05 10:47:31 +0200

    Added toast displaying answer

--- Task2/app/src/main/java/com/example/task2/RandomNumberActivity.kt
+++ Task2/app/src/main/java/com/example/task2/RandomNumberActivity.kt
@@ -1,9 +1,9 @@
 package com.example.task2
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 class RandomNumberActivity : Activity() {
 
@@ -13,6 +13,8 @@ class RandomNumberActivity : Activity() {
 
         val maxValue = intent.getIntExtra("maxValue", 1)
         val generatedNumber = (0..maxValue).random()
+
+        Toast.makeText(this, "Tilfeldig tall: $generatedNumber", Toast.LENGTH_LONG).show()
 
         val textView = findViewById<TextView>(R.id.random_number)
         val result = "Generert tall: $generatedNumber"
