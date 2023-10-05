43e03e292fd6cf5a6d7b841a9c0c197d3517ec8f
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-05 11:21:20 +0200

    Modified MovieListFragment to use a list of Movie objects instead of strings.xml

--- Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
+++ Task4/app/src/main/java/com/example/task4/MovieListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.task4
 
+import Movie
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,15 +11,29 @@ import android.widget.ListView
 
 class MovieListFragment : Fragment() {
 
+    companion object {
+        private const val MOVIE_LIST_KEY = "movieList"
+
+        fun newInstance(movies: ArrayList<Movie>): MovieListFragment {
+            val fragment = MovieListFragment()
+            val bundle = Bundle()
+            bundle.putParcelableArrayList(MOVIE_LIST_KEY, movies)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
         val listView: ListView = view.findViewById(R.id.movieList)
-        val movies = resources.getStringArray(R.array.movies)
 
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, movies)
+        val movies: ArrayList<Movie>? = arguments?.getParcelableArrayList(MOVIE_LIST_KEY)
+        val movieNames = movies?.map { it.name }?.toTypedArray() ?: arrayOf()
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, movieNames)
         listView.adapter = adapter
 
         return view
