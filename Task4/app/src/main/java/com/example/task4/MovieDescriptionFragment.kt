b8e2e517693e6f3405e3ed010230a514a315efc8
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-05 11:55:37 +0200

    Added dynamic image display

--- Task4/app/src/main/java/com/example/task4/MovieDescriptionFragment.kt
+++ Task4/app/src/main/java/com/example/task4/MovieDescriptionFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MovieDescriptionFragment : Fragment() {
 
@@ -32,9 +33,14 @@ class MovieDescriptionFragment : Fragment() {
 
         val movie: Movie? = arguments?.getParcelable(MOVIE_DESCRIPTION_KEY)
 
-        // TODO use image from Movie object instead of batman
-        imageView.setImageResource(R.drawable.batman)
+        val resourceId = context?.resources?.getIdentifier(movie?.image, "drawable", context?.packageName)
+        if (resourceId != null) {
+            imageView.setImageResource(resourceId)
+        } else {
+            Toast.makeText(requireContext(), "Could not load Image!", Toast.LENGTH_LONG).show()
+        }
 
+        // If there isn't any description, we add an error value
         description.text = movie?.description ?: "Description not available"
 
         return view
