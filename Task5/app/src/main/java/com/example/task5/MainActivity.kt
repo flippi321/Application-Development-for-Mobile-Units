145c1823a0833dec26b310e0aea2d4bcdef5732a
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-06 21:49:41 +0200

    Updated UI to actually send and recieve the relevant information

--- Task5/app/src/main/java/com/example/task5/MainActivity.kt
+++ Task5/app/src/main/java/com/example/task5/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.task5
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
@@ -17,73 +16,72 @@ const val URL = "https://bigdata.idi.ntnu.no/mobil/tallspill.jsp"
 
 class MainActivity : AppCompatActivity() {
     private val network: HttpWrapper = HttpWrapper(URL)
-    //private val network: HttpWrapper = HttpWrapper(URL_JSON)
+
+    private lateinit var nameEditText: EditText
+    private lateinit var cardNumberEditText: EditText
+    private lateinit var guessEditText: EditText
+    private lateinit var submitButton: Button
+    private lateinit var resultTextView: TextView
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menu.add("Try new").setShowAsAction(SHOW_AS_ACTION_ALWAYS)
-        menu.add("Guess").setShowAsAction(SHOW_AS_ACTION_ALWAYS)
-        menu.add("GET (header)").setShowAsAction(SHOW_AS_ACTION_ALWAYS)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.title) {
-            "Try new" -> performRequest(HTTP.GET, requestParameters())
-            "Guess" -> performRequest(HTTP.GET, sendNumber())
-            "GET (header)" -> performRequest(HTTP.GET_WITH_HEADER,
-                requestParameters())
-            else -> return false
+
+        nameEditText = findViewById(R.id.nameEditText)
+        cardNumberEditText = findViewById(R.id.cardNumberEditText)
+        guessEditText = findViewById(R.id.guessEditText)
+        submitButton = findViewById(R.id.submitButton)
+        resultTextView = findViewById(R.id.result)
+
+        submitButton.setOnClickListener {
+            if (nameEditText.isEnabled && cardNumberEditText.isEnabled) {
+                performRequest(HTTP.GET, requestParameters())
+            } else {
+                performRequest(HTTP.GET, sendNumber())
+            }
         }
-        return true
     }
+
     private fun requestParameters(): Map<String, String> {
-        val firstName = "Test"
-        val cardNumber = "123"
+        val firstName = nameEditText.text.toString()
+        val cardNumber = cardNumberEditText.text.toString()
+
+        nameEditText.isEnabled = false
+        cardNumberEditText.isEnabled = false
 
         return mapOf(
             "navn" to firstName,
             "kortnummer" to cardNumber,
         )
     }
+
     private fun sendNumber(): Map<String, String> {
-        val number = "5"
+        val number = guessEditText.text.toString()
 
         return mapOf(
             "tall" to number
         )
     }
-    private fun performRequest(typeOfRequest: HTTP, parameterList:
-    Map<String, String>) {
+
+    private fun performRequest(typeOfRequest: HTTP, parameterList: Map<String, String>) {
         CoroutineScope(IO).launch {
             val response: String = try {
                 when (typeOfRequest) {
                     HTTP.GET -> network.get(parameterList)
-                    HTTP.POST -> network.post(parameterList)
-                    HTTP.GET_WITH_HEADER ->
-                        network.getWithHeader(parameterList)
+                    HTTP.POST -> TODO()
+                    HTTP.GET_WITH_HEADER -> TODO()
                 }
             } catch (e: Exception) {
                 Log.e("performRequest()", e.message!!)
                 e.toString()
             }
-            // Endre UI paa hovedtraaden
             MainScope().launch {
                 setResult(response)
-            //setResult(formatJsonString(response))
             }
         }
     }
+
     private fun setResult(response: String?) {
-        findViewById<TextView>(R.id.result).text = response
-    }
-    private fun formatJsonString(str: String): String {
-        return try {
-            JSONArray(str).toString(4)
-        } catch (e: Exception) {
-            Log.e("formatJsonString()", e.toString())
-            e.message!!
-        }
+        resultTextView.text = response
     }
 }
