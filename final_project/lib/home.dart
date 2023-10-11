a358ec6ae1b46fd79fe64a4016642176e6ce6aa1
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-11 17:10:33 +0200

    Created a new homepage for the soduku app

---
+++ final_project/lib/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SudokuHomePage extends StatelessWidget {
+  const SudokuHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.orange, Colors.blue],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 50.0,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Sodukoid",
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO implement "New Game" logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: const Text('New Game'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO implement "How to play" logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: const Text('How to Play'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
