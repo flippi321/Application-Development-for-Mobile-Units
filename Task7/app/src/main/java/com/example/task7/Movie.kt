c84f1d7dc06e4b237271b401583310fd720e3a23
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-11-08 21:47:18 +0100

    Added a toJson

--- Task7/app/src/main/java/com/example/task7/Movie.kt
+++ Task7/app/src/main/java/com/example/task7/Movie.kt
@@ -1,10 +1,25 @@
 package com.example.task7
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 data class Movie(
     val name: String,
     val director: String,
     val actors: List<String>
 ) {
+    fun toJson(): JSONObject {
+        val jsonObject = JSONObject()
+        jsonObject.put("name", name)
+        jsonObject.put("director", director)
+        val actorsArray = JSONArray()
+        actors.forEach { actor ->
+            actorsArray.put(actor)
+        }
+        jsonObject.put("actors", actorsArray)
+        return jsonObject
+    }
+
     override fun toString(): String {
         return "Movie(name='$name', director='$director', actors=${actors.joinToString(", ")})"
     }
