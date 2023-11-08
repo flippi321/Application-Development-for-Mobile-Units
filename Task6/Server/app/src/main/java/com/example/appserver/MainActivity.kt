a21258a94671d8415873c2b24ec69b4f7a626fb0
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-11-08 10:53:55 +0100

    Created a basic Main File using a server class

--- Task6/Server/app/src/main/java/com/example/appserver/MainActivity.kt
+++ Task6/Server/app/src/main/java/com/example/appserver/MainActivity.kt
@@ -1,46 +1,16 @@
 package com.example.appserver
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appserver.ui.theme.AppServerTheme
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppServerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppServerTheme {
-        Greeting("Android")
+        // Start the server class
+        Server(this, textView).start()
     }
 }
