7ab069039c343eaecc6d939551198b848754b7c9
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-11-08 11:05:06 +0100

    Added event listener so that messages can be sent to the client

--- Task6/Server/app/src/main/java/com/example/appserver/MainActivity.kt
+++ Task6/Server/app/src/main/java/com/example/appserver/MainActivity.kt
@@ -2,15 +2,36 @@ package com.example.appserver
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : Activity() {
+    private lateinit var server: AppServer
+    private lateinit var buttonSend: Button
+    private lateinit var editTextInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.textView)
 
-        // Start the server class
-        AppServer(textView).start()
+        val textView = findViewById<TextView>(R.id.textView)
+        editTextInput = findViewById(R.id.editTextInput)
+        buttonSend = findViewById(R.id.buttonSend)
+
+        // Initialize the server
+        server = AppServer(textView)
+
+        // Set up the button event listener
+        buttonSend.setOnClickListener {
+            val message = editTextInput.text.toString()
+            if (message.isNotBlank()) {
+                server.sendMessageToClient(message)
+                editTextInput.text.clear()
+            }
+        }
+
+        // Start the server
+        server.start()
     }
 }
